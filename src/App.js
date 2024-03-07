417108a832b2437b4bec6a6cd7283dcfb7ffd932
Arul-1911 <arulpraveen19112002@gmail.com>
2024-03-07 19:21:06 +0530

    sample react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+
+function onclickME() {
+  console.log("running");
+}
+
+
 
 function App() {
+  const [count, setCount] = useState(0);
+  function updateCounter() {
+    setCount(count+1)
+  }
+  const courses = [
+    {
+      id: 1,
+      course: "pythion",
+      price: 500,
+    },
+    { id: 2, course: "pythion", price: 500 },
+    { id: 3, course: "pythion", price: 500 },
+    { id: 4, course: "pythion", price: 500 },
+  ];
+
+  const listItems = courses.map((course) => (
+    <li key={course.id}>
+      {course.course} {course.price}
+    </li>
+  ));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ul>{listItems}</ul>
+      <hr />
+      <div>
+        <button onClick={onclickME}>click me</button>
+      </div>
+      <div>
+        <p>counter</p>
+        <button onClick={updateCounter}>{count}</button>
+      </div>
+    </>
   );
 }
 
